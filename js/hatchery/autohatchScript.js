e06615bc04a1ab0dae56bbcb5edb2232730c2006
Kevin POIROT <kevin.poirot@epitech.eu>
2022-05-27 23:45:08 +0200

    fix(hatch): default to breeding by desc order

--- js/hatchery/autohatchScript.js
+++ js/hatchery/autohatchScript.js
@@ -1,5 +1,7 @@
 intervalMap.set('autohatchInterval', setInterval(() => {
     if (App.game.breeding.hasFreeQueueSlot() || App.game.breeding.hasFreeEggSlot()) {
+        Settings.setSettingByName('hatcherySort', SortOptions[SortOptions[6]]);
+        Settings.setSettingByName('hatcherySortDirection', true);
         const firstAvailable = PartyController.getHatcherySortedList()?.find(pokemon => pokemon.breeding === false);
         App.game.breeding.addPokemonToHatchery(firstAvailable);
     }
