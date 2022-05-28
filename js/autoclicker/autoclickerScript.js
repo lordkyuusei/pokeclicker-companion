cac83b2db0278f17c41cfb510fe789a75e1988b6
Kevin POIROT <kevin.poirot@epitech.eu>
2022-05-28 15:00:14 +0200

    fix(autoclick): only clicks when battling + change click method for gyms

--- js/autoclicker/autoclickerScript.js
+++ js/autoclicker/autoclickerScript.js
@@ -1,3 +1,24 @@
+/* Gamestates:
+    0: "idle"
+    1: "paused"
+    2: "fighting"
+    3: "gym"
+    4: "dungeon"
+    5: "safari"
+    6: "town"
+    7: "shop"
+    8: "battleFrontier"
+    9: "temporaryBattle" */
+
 if (App.game && BattleFrontierRunner.started() === false) {
-    intervalMap.set('autoclickInterval', setInterval(() => Battle.clickAttack(), 100));
+    intervalMap.set('autoclickInterval', setInterval(() => {
+        const battling = {
+            2: () => Battle.clickAttack(),
+            3: () => GymBattle.clickAttack(),
+        }
+        const func = battling[App.game.gameState]
+        if (func) {
+            func()
+        }
+    }, 100));
 }
