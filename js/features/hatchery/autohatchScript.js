7fd6afb4d3bb579764aefd8d892402d79a79aa0c
Kevin POIROT <kevin.poirot@epitech.eu>
2022-06-02 00:01:12 +0200

    fix(hatchery): fix an issue causing pokemon not to be sorted property

--- js/features/hatchery/autohatchScript.js
+++ js/features/hatchery/autohatchScript.js
@@ -4,15 +4,15 @@ intervalMap.set('autohatchInterval', setInterval(() => {
     const sortOption = parseInt(intervalMap.get('hatchOption'));
     const region = parseInt(intervalMap.get('regionOption'));
     if (App.game && App.game.breeding.canAccess() && App.game.breeding.hasFreeEggSlot()) {
-        let allPokemon = [...App.game.party.caughtPokemon];
-
         if (sortOption !== Settings.getSetting('hatcherySort').value) {
-            allPokemon = allPokemon.sort(PartyController.compareBy(Settings.getSetting('hatcherySort').observableValue(), Settings.getSetting('hatcherySortDirection').observableValue()))
             Settings.setSettingByName('hatcherySort', SortOptions[SortOptions[parseInt(sortOption)]]);
             Settings.setSettingByName('hatcherySortDirection', true);
         }
 
-        const breedable = allPokemon.filter(pokemon => pokemon.breeding === false && pokemon.level === 100);
+        let breedable = [...App.game.party.caughtPokemon]
+            .sort(PartyController.compareBy(Settings.getSetting('hatcherySort').observableValue(), Settings.getSetting('hatcherySortDirection').observableValue()))
+            .filter(pokemon => pokemon.breeding === false && pokemon.level === 100);
+
         if (breedable) {
             const firstAvailable = region >= 0 ? breedable.find(pokemon => PokemonHelper.calcNativeRegion(pokemon.name) === region) : breedable[0];
             if (firstAvailable) {
